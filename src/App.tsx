439d9e57cbc098462a304dada905a49848bfa8ba
Simon Cragg <22366916+simoncragg@users.noreply.github.com>
2024-08-26 19:19:30 +0100

    style: landing page

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,13 @@ import './App.css'
 const App = () => {
   
   return (
-    <>
-      <h1>Checkmate Arena</h1>
-      <Chessboard id="defaultBoard" />
-      <p>
-        <a>New Game</a>
-      </p>
-    
-    </>
+    <div className="flex flex-col gap-4">
+      <h1 className="text-4xl font-bold">Checkmate Arena</h1>
+      <div>
+        <Chessboard id="defaultBoard" />
+      </div>
+      <button type="button" className="border-solid border-2 rounded-md">New Game</button>
+    </div>
   )
 }
 
