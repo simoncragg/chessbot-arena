30742401c1223b0139e4fd633240721faddb6bef
Simon Cragg <22366916+simoncragg@users.noreply.github.com>
2024-08-26 19:50:25 +0100

    feat: add basic page routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import { Chessboard } from 'react-chessboard'
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
 
 const App = () => {
   
   return (
-    <div className="flex flex-col gap-4">
-      <h1 className="text-4xl font-bold">Checkmate Arena</h1>
-      <div>
-        <Chessboard id="defaultBoard" />
-      </div>
-      <button type="button" className="border-solid border-2 rounded-md">New Game</button>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,17 @@
+import { Chessboard } from 'react-chessboard'
+import '../App.css'
+
+const LandingPage = () => {
+  
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="text-4xl font-bold">Checkmate Arena</h1>
+      <div>
+        <Chessboard id="defaultBoard" />
+      </div>
+      <button type="button" className="border-solid border-2 rounded-md">New Game</button>
+    </div>
+  )
+}
+
+export default LandingPage;
