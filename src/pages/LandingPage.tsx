ba4a0cadf2c7913660cf5a3e7801137c1f10790b
Simon Cragg <22366916+simoncragg@users.noreply.github.com>
2024-08-29 22:53:15 +0100

    style: update landing page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -4,10 +4,15 @@ const LandingPage = () => {
   
   return (
     <div className="flex flex-col items-center gap-8 w-5/6">
-      <h1 className="text-4xl font-bold">Checkmate Arena</h1>
+      <h1 className="font-cinzel text-4xl font-bold">Chess Bot Arena</h1>
       <div className="flex flex-col w-full md:w-96 gap-8">
         <Chessboard id="defaultBoard" />
-        <button type="button" className="border-solid border-2 rounded-md p-2 w-full">New Game</button>
+        <button 
+          type="button" 
+          className="font-cinzel text-xl border-solid border-2 border-stone-300 bg-green-800 rounded-md p-2 w-full"
+        >
+          New Game
+        </button>
       </div>
     </div>
   )
