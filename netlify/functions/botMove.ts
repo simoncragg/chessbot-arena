d27185d9bf8be7c43297cd3a1b16b0fa5f90a876
Simon Cragg <22366916+simoncragg@users.noreply.github.com>
2024-09-01 20:13:27 +0100

    feat: implement botMove function
    This will be used as the default chessbot

--- netlify/functions/botMove.ts
+++ netlify/functions/botMove.ts
@@ -1,10 +1,44 @@
-import type { Context } from "@netlify/functions";
+import { v4 as uuidv4 } from 'uuid';
+import { Chess } from "chess.js";
 
-const botMove = async (req: Request, context: Context) => {
-  console.log("body", req.body);
-  console.log("site: ", context.site);
-  return new Response("Hello, world!")
-}
+const botMove = async (request: Request) => {
+
+  try {
+
+    if (request.method !== "POST") {
+      return new Response(JSON.stringify({ message: "Method Not Allowed" }), { status: 405 });
+    }
+
+    const body = await new Response(request.body).text();
+    const { fen } = JSON.parse(body);
+
+    if (!fen) {
+      return new Response(JSON.stringify({ message: "Bad Request: Missing FEN notation" }), { status: 400 });
+    }
+
+    const chess = new Chess(fen);
+
+    if (!chess) {
+      return new Response(JSON.stringify({ message: "Bad Request: Invalid FEN notation" }), { status: 400 });
+    }
+
+    const moves = chess.moves();
+    if (moves.length === 0) {
+      return new Response(JSON.stringify({ message: "No valid moves available" }), { status: 400 });
+    }
+
+    const move = moves[Math.floor(Math.random() * moves.length)];
+
+    return new Response(JSON.stringify({ move }), { status: 200 });
+
+  } catch (error) {
+    const errorId = uuidv4();
+    console.error(error.message, $`Error ID: "${errorId}"`);
+
+    return new Response(JSON.stringify({
+      message: `Internal Server Error. See logs for more details. Error ID: "${errorId}"`
+    }), { status: 500 });
+  }
+};
 
 export default botMove;
-
