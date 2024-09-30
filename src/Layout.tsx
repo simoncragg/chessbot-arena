5dad048b51fa066f994d6366990b59604140a200
Simon Cragg <22366916+simoncragg@users.noreply.github.com>
2024-09-30 23:54:47 +0100

    feat: add footer

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,11 +1,13 @@
+/// <reference types="vite-plugin-svgr/client" />
 import { Link, useOutlet } from "react-router-dom";
+import Netlify from "./assets/netlify.svg?react";
 
 const Layout = () => {
 
   const outlet = useOutlet();
 
   return (
-    <div className="flex flex-col items-center max-w-full">
+    <div className="flex flex-col items-center max-w-full gap-4 md:gap-8">
 
       <header className="flex flex-col justify-center items-center bg-gradient-to-b bg-zinc-950 w-full h-24">
         <Link to="/">
@@ -13,9 +15,15 @@ const Layout = () => {
         </Link>
       </header>
 
-      <div className="mt-4 md:mt-8">
-        {outlet}
-      </div>
+      {outlet}
+
+      <footer className="flex flex-col items-center mb-8 gap-8 w-full">
+        <Netlify />
+        <div className="flex flex-col items-center">
+          <span className="text-base text-gray-200">Released under the MIT License.</span>
+          <span className="text-base text-gray-200">Copyright © 2024-Present Simon Cragg</span>
+        </div>
+      </footer>
     </div>
   );
 }
