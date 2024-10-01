27a6ad313a203f6f0980176b727beb62557aebf9
Simon Cragg <22366916+simoncragg@users.noreply.github.com>
2024-10-01 21:21:13 +0100

    feat: integrate Cockatiel retry policy into Netlify functions

--- src/services/getBots.ts
+++ src/services/getBots.ts
@@ -1,14 +1,18 @@
 import type { ChessBot } from "../types";
+import { handleAll, retry } from "cockatiel";
 
 async function getBots(callback: (chessBots: ChessBot[]) => void) {
-
-  const response = await fetch("/.netlify/functions/getBots", {
-    method: "GET",
-    headers: { "Content-type": "application/json" },
-  });
-
+  const retryPolicy = retry(handleAll, { maxAttempts: 2 });
+  const response = await retryPolicy.execute(() => fetchBots());
   const chessBots = await response.json();
   callback(chessBots);
+}
+
+async function fetchBots() {
+  return await fetch("/.netlify/functions/getBots", {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  });
 }
 
 export default getBots;
--- src/services/getNextMove.ts
+++ src/services/getNextMove.ts
@@ -1,18 +1,29 @@
+import { handleAll, retry } from "cockatiel";
+
+interface GetNextMoveRequest {
+  fen: string;
+  botId: string;
+}
+
 interface GetNextMoveResult {
   move: string;
   fen: string;
 }
 
-async function getNextMove(args: { fen: string; botId: string }, callback: (result: GetNextMoveResult) => void) {
-  const { fen, botId } = args;
-  const response = await fetch("/.netlify/functions/botMove", {
-    method: "POST",
-    headers: { "Content-type": "application/json" },
-    body: JSON.stringify({ fen, botId }),
-  });
-
+async function getNextMove(request: GetNextMoveRequest, callback: (result: GetNextMoveResult) => void) {
+  const { fen } = request;
+  const retryPolicy = retry(handleAll, { maxAttempts: 2 });
+  const response = await retryPolicy.execute(() => fetchMove(request));
   const result = await response.json();
   callback({ move: result.move, fen });
+}
+
+async function fetchMove(request: GetNextMoveRequest): Promise<Response> {
+  return await fetch("/.netlify/functions/botMove", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(request),
+  });
 }
 
 export default getNextMove;
