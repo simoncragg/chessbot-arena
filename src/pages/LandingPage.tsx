a8da3da40b6a169f427379527491caebaf72a812
Simon Cragg <22366916+simoncragg@users.noreply.github.com>
2024-10-03 00:31:16 +0100

    style: tweak landing page styling

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -6,7 +6,7 @@ const LandingPage = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex flex-col items-center w-full bg-neutral-900 gap-8 mt-4 md:mt-4">
+    <div className="flex flex-col items-center w-full bg-neutral-900 gap-6 mt-4 md:mt-4">
 
       <div className="flex justify-center bg-neutral-900 w-full">
 
@@ -14,7 +14,7 @@ const LandingPage = () => {
             <Chessboard isDraggablePiece={() => false} />
             <button 
               type="button" 
-              className="font-cinzel font-semibold text-xl bg-green-700 border-b-4 border-green-900 rounded-md p-2 w-full"
+              className="font-cinzel font-semibold text-xl bg-green-700 hover:bg-green-800 border-b-4 border-green-900 rounded-md p-2 w-full"
               onClick={() => navigate("/new-game")}
             >
               New Game
